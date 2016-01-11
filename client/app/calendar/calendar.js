ac149b0a0ddd23547443aed8b72b24f5830fb7de
Aaron Spafford <aaron.spafford@gmail.com>
2016-01-10 23:37:19 -0800

    format dates

--- client/app/calendar/calendar.js
+++ client/app/calendar/calendar.js
@@ -10,12 +10,28 @@
       $scope.showCalendar = function() {
         Calendar.getTimeslots(day)
           .then(function(data) {
-            $scope.data.timeslots = data.timeslots;
+            var formattedData = formatDates(data);
+            $scope.data.fullDate = formattedData.fullDate;
+            $scope.data.timeslots = formattedData.timeslots;
           })
           .catch(function(error) {
             console.error(error);
           });
       };
       $scope.showCalendar();
+
+      var formatDates = function(data) {
+
+        var fullDateFormat = 'MM/DD/YYYY';
+        var timeslotFormat = 'h:mma';
+
+        data.fullDate = moment(data.timeslots[0]['start']).format(fullDateFormat);
+
+        data.timeslots.forEach(function(item, index) {
+          data.timeslots[index]['start'] = moment(item.start).format(timeslotFormat);
+          data.timeslots[index]['end'] = moment(item.end).format(timeslotFormat);
+        });
+        return data;
+      };
     });
 })();
